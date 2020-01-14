a38a7168007bf9c5a0e17c9576652b8355668439
jacobrammer <jrammer101@gmail.com>
2020-01-14 12:47:04 -0800

    1-3

--- Assignment1/1-3.c
+++ Assignment1/1-3.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+/*
+ * Jacob Rammer
+ * 1-3
+ */
+
+
+int evenBit(unsigned int n)
+{
+    /*
+     * accomplish this by masking the even bits only. Then compare them to not
+     * 0 (0x00000001) and then not it which will leave the final word as a 0 or 1
+     * which is then returned
+     */
+
+    return !!(n & 0x55555555);
+}
+
 
 int main()
 {
-    /*
-     * mask the even bits and use !!
+    /* From lab
+     * mask the even bits and use !! 5: 0101
      */
+
+
+    printf("%d", evenBit(0xFFFFFFFF));
+    return 0;
 }
