80250459c7cddc68a5f7c6b373fa11449ee9e784
Jacob <jrammer101@gmail.com>
2020-01-20 12:53:22 -0800

    2-2

--- Assignment2/2-2.c
+++ Assignment2/2-2.c
@@ -7,12 +7,22 @@
 
 unsigned int extract(unsigned int x, int i)
 {
-    return 0;
+    /* from lab 1
+     * 0xff << (3 << 3) = 3 * 8 = 24
+     */
+    unsigned int leftOffset =  24 - (i << 3);  //from lab 1: i << 3 => i * 3
+    int newWord = x << leftOffset; // shift the bits to position indicated above
+    return (unsigned int) (newWord >> 24);
+    /*              ^^^^^^
+     * From Piazza: convert unsigned ints to signed, then return as unsigned
+     * casting the newWord as an unsigned int then shifting the wanted bits to
+     * the beginning of the word.
+     */
 }
 
 
 int main()
 {
-
+    printf("%#010x\n", extract(0x8F000000, 3));
     return 0;
 }
