03782d7b7a29e504449c1bd567ca2c46cc4ca17f
Jacob <jrammer101@gmail.com>
2020-02-02 10:39:03 -0800

    3-2

--- Assignment3/3-2.c
+++ Assignment3/3-2.c
@@ -5,13 +5,8 @@
 
 #include <stdio.h>
 
-long f(long long a, long long b, long long c)
+long f(long long a, long long b, long long c)  // using long long because of gcc warnings
 {
-    /*
-     * c = rdx
-     * b = rdi
-     * a = rdi
-     */
     c = c + b;  // addq %rsi, %rd (addition)
     a = a * c;  // imulq % rdx, %rdi (multiplication)
     c = c << 63;  // salq $63, %rdx
@@ -24,4 +19,5 @@ int main()
 {
     printf("%ld\n", f(1, 2, 4));
     printf("%ld\n", f(3, 5, 7));
+    printf("%ld\n", f(10, 20, 30));
 }
