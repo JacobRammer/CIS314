14e11ab6c0e0ca0c10f79eb396876e35a87884e6
Jacob <jrammer101@gmail.com>
2020-02-08 14:23:29 -0800

    4-3

--- Assignment4/4-3.c
+++ Assignment4/4-3.c
@@ -5,14 +5,35 @@
 
 #include <stdio.h>
 
-long fact(long x)
+long fact(long x)  // x is a callee-saved variable, which will be stored in %rbx
 {
     if(x <= 1)
         return 1;
-    long px = x;
+//    long px = x;
     long fx = fact(x - 1);
+    /*      ^^
+     * Since this is a recursive call, a temporary register will be used, which is %rbx
+     */
     return px * fx;
 }
+
+/*
+ * fact:
+.LFB11:
+	.cfi_startproc
+	cmpq	$1, %rdi
+	jle	.L3
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movq	%rdi, %rbx
+	leaq	-1(%rdi), %rdi
+	call	fact
+	imulq	%rbx, %rax
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+ */
 
 int main()
 {
