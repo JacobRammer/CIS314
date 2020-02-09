eeabcc90417d198b2b88fc86c02e20f71f281ef8
Jacob <jrammer101@gmail.com>
2020-02-09 14:51:33 -0800

    4-2

--- Assignment4/4-4.c
+++ Assignment4/4-4.c
@@ -18,11 +18,65 @@ void transpose(array_t a) {
          }
      }
 }
+
+void optimizedTranspose(array_t a)  // this mainly follows the slides from class
+{
+    /*
+    * A[i][j] is element of type T, which requires K bytes
+    * Address  A + i * (C * K) +  j * K 
+    */
+
+
+    for(int i = 0; i < N; i++)
+    {
+        long *col = &a[0][i];  // hint from slides for index for both row and column 
+        long *row = &a[i][0];
+        for(int j = 0; j < i; j++)
+        {            
+            long temp1 = *row;
+            long temp2 = *col;
+            *row = temp2;  // will swap column with row
+            *col = temp1;  // swap row with column
+            row++;
+            col += N;
+            /*
+            * Add 1 to row (which will be 1 * 8) to the location of row
+            *  Increment column by 4 (4 * 8) to get to the next list
+            * {{1, 2, 3, 4}
+            * {5, 6, 7, 8}
+            * {9, 10, 11, 12}
+            * {13, 14, 15, 16}}
+            */
+        }
+    }
+}
+
+void testPrint(array_t a)
+{
+    for(int i = 0; i < N; i++)
+    {
+        for(int j = 0; j < N; j++)
+            printf("%ld ", a[i][j]);
+        printf("\t");
+    }
+}
 int main()
 {
-    long test[7][7] = {{5, 4, 3, 2, 1}, {9, 0}};
-    long *tt = *(test + 1);
-    printf("test %ld\n", *tt);
-    printf("test %ld\n", **(test + 8));
+    array_t test = { {1, 2, 3, 4},{5, 6, 7, 8},{9, 10, 11, 12},{13, 14, 15, 16} };
+
+    optimizedTranspose(test);
+    testPrint(test);
     return 0;
 }
+
+/*
+L3:
+movq (%rax), %rcx
+movq (%rdx), %rsi
+movq %rsi, (%rax)
+movq %rcx, (%rdx)
+addq $8, %rax
+addq $32, %rdx
+cmpq %r9, %rax
+jne .L3
+*/
